920530dd7f0858d818a6b3fb515c66a9c2cd6598
Stephen Turton <stephenturton@gmail.com>
2013-06-30 17:13:47 -0400

    Test Change Two

---
+++ TestOne/MyClassTwo.h
@@ -0,0 +1,13 @@
+//
+//  MyClassTwo.h
+//  TestOne
+//
+//  Created by Stephen Turton on 06/30/13.
+//  Copyright (c) 2013 Stephen Turton. All rights reserved.
+//
+
+
+
+@interface MyClassTwo : NSObject
+
+@end
