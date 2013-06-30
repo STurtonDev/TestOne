2639d7acc31e078f934af7586f1a6304a6d292ef
STurtonDev <stephen@sturtondev.com>
2013-06-30 18:04:53 -0400

    Testing

---
+++ TestOne/MyClassFive.h
@@ -0,0 +1,13 @@
+//
+//  MyClassFive.h
+//  TestOne
+//
+//  Created by Stephen Turton on 6/30/13.
+//  Copyright (c) 2013 Stephen Turton. All rights reserved.
+//
+
+
+
+@interface MyClassFive : NSObject
+
+@end
---
+++ TestOne/MyClassFive.m
@@ -0,0 +1,13 @@
+//
+//  MyClassFive.m
+//  TestOne
+//
+//  Created by Stephen Turton on 6/30/13.
+//  Copyright (c) 2013 Stephen Turton. All rights reserved.
+//
+
+#import "MyClassFive.h"
+
+@implementation MyClassFive
+
+@end
