9190495eda47c6fa30450600ed26deb7e70a5f61
Stephen Turton <stephenturton@gmail.com>
2013-06-30 17:56:48 -0400

    Add My Class Four

---
+++ TestOne/MyClassFour.h
@@ -0,0 +1,13 @@
+//
+//  MyClassFour.h
+//  TestOne
+//
+//  Created by Stephen Turton on 06/30/13.
+//  Copyright (c) 2013 Stephen Turton. All rights reserved.
+//
+
+
+
+@interface MyClassFour : NSObject
+
+@end
---
+++ TestOne/MyClassFour.m
@@ -0,0 +1,13 @@
+//
+//  MyClassFour.m
+//  TestOne
+//
+//  Created by Stephen Turton on 06/30/13.
+//  Copyright (c) 2013 Stephen Turton. All rights reserved.
+//
+
+#import "MyClassFour.h"
+
+@implementation MyClassFour
+
+@end
